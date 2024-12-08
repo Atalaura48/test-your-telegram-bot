15a0665d6f56f137dd3762a3b7b44bfcb90eb95a
Doğukan <dogukanatalay@posta.mu.edu.tr>
2024-12-08 14:04:54 +0300

    Languages added

---
+++ diger/tr.py
@@ -0,0 +1,17 @@
+def get_messages():
+    return {
+        'enter_token': "Lütfen bot tokenini girin: ",
+        'invalid_token': "Hata: Geçersiz token sağlandı. Lütfen bot tokenini kontrol edin ve tekrar deneyin.",
+        'no_updates': "Herhangi bir değişiklik yok. Üsttekini yapana kadar kontrol edilecek…",
+        'first_message': "Lütfen bota önce bir mesaj gönderin ya da /start komutunu kullanın.",
+        'test_message_prompt': "Botun çalışıp çalışmadığını denemek etmek ister misiniz? (evet/hayır): ",
+        'test_message_sent': "Test mesajı başarıyla gönderildi.",
+        'error_sending_message': "Mesaj gönderilirken bir hata oluştu: ",
+        'exiting': "Çıkılıyor…",
+        'working_message': "Merhaba 👋 Ben doğru şekilde çalışıyorum 😊",
+        'your_chat_id': "Sohbet ID'niz: ",
+        'bots_user_id': "Botun Kullanıcı ID'si: ",
+        'yes': "evet",
+        'no': "hayır",
+        'invalid_input': "Geçersiz değer. Lütfen 'evet' ya da 'hayır' olarak yanıt verin."
+    }
