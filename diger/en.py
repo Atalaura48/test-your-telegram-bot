2ed3f8e5fe6955edb4529fd55d762e63cbbe1c0b
Doğukan <dogukanatalay@posta.mu.edu.tr>
2024-12-08 14:04:01 +0300

    Create en.py

---
+++ diger/en.py
@@ -0,0 +1,17 @@
+def get_messages():
+    return {
+        'enter_token': "Please enter your bot token: ",
+        'invalid_token': "Error: Invalid token provided. Please check your bot token and try again.",
+        'no_updates': "No new updates. It will be checked until you do it…",
+        'first_message': "Please send a message to the bot first or /start it.",
+        'test_message_prompt': "Do you want to test if the bot is working? (yes/no): ",
+        'test_message_sent': "Test message sent successfully.",
+        'error_sending_message': "An error occurred while sending the message: ",
+        'exiting': "Exiting…",
+        'working_message': "Hello 👋 I'm working correctly 😊",
+        'your_chat_id': "Your Chat ID: ",
+        'bots_user_id': "Bot's User ID: ",
+        'yes': "yes",
+        'no': "no",
+        'invalid_input': "Invalid input. Please respond with 'yes' or 'no'."
+    }
