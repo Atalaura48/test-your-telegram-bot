d85384be1c0586a98630371d050d84c87c907621
Doğukan <dogukanatalay@posta.mu.edu.tr>
2024-12-08 14:04:18 +0300

    Create fr.py

---
+++ diger/fr.py
@@ -0,0 +1,17 @@
+def get_messages():
+    return {
+        'enter_token': "Veuillez entrer votre token de bot: ",
+        'invalid_token': "Erreur: Token invalide. Veuillez vérifier votre token et réessayer.",
+        'no_updates': "Pas de nouvelles mises à jour. Cela sera vérifié jusqu'à ce que vous envoyiez un message…",
+        'first_message': "Veuillez envoyer un message au bot ou démarrer avec /start.",
+        'test_message_prompt': "Voulez-vous tester si le bot fonctionne ? (oui/non): ",
+        'test_message_sent': "Message de test envoyé avec succès.",
+        'error_sending_message': "Une erreur est survenue lors de l'envoi du message: ",
+        'exiting': "Quitter…",
+        'working_message': "Bonjour 👋 Je fonctionne correctement 😊",
+        'your_chat_id': "Votre ID de chat: ",
+        'bots_user_id': "ID utilisateur du bot: ",
+        'yes': "oui",
+        'no': "non",
+        'invalid_input': "Entrée invalide. Veuillez répondre par 'oui' ou 'non'."
+    }
